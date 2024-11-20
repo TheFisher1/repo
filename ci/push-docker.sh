e55efd83723b5861aba663be6f76110a0438d362
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2024-11-20 16:23:56 +0200

    save file 2

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Login to Docker Hub (non-interactive mode)
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+# Push both tags
 docker push "$DOCKER_USERNAME/micronaut-app:1.0-${GIT_COMMIT::8}" 
 docker push "$DOCKER_USERNAME/micronaut-app:latest" &
 wait
