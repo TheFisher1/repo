bb21f41b79d71df896353f4ccf251a8c6263cbc1
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2024-11-20 16:11:22 +0200

    save file 2

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-docker push "ghcr.io/$GITHUB_ACTOR/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$GITHUB_ACTOR/micronaut-app:latest" &
+docker push "ghcr.io/$DOCKER_USERNAME/micronaut-app:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$DOCKER_USERNAME/micronaut-app:latest" &
 wait
