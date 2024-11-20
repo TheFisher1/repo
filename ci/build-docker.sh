2268edde04286e0a6afd3b227b5c8d15569e331b
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2024-11-20 17:11:11 +0200

    save file 2

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
+LOWERCASE_USER=$(echo "$GITHUB_USER" | tr '[:upper:]' '[:lower:]')
+
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-REPO="ghcr.io/$docker_username/"
+REPO="ghcr.io/${LOWERCASE_USER}/"
 echo "${REPO}"
 docker build -t "${REPO}micronaut-app:latest" -t "${REPO}micronaut-app:1.0-$Tag" app/
