25b6a5b35ed14895b989644be6ca5047c9bdb18d
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2024-11-20 17:08:28 +0200

    save file 2

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "$GITHUB_TOKEN" | docker login ghcr.io -u $GITHUB_USER --password-stdin
+# Convert username to lowercase
+LOWERCASE_USER=$(echo "$GITHUB_USER" | tr '[:upper:]' '[:lower:]')
+
+# Login to GHCR with PAT
+echo "$GITHUB_TOKEN" | docker login ghcr.io -u $LOWERCASE_USER --password-stdin
+
+# Show available images (for debugging)
 docker images
 
-docker push "ghcr.io/${docker_username}/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/${docker_username}/micronaut-app:latest" &
+# Push to GHCR using lowercase username
+docker push "ghcr.io/${LOWERCASE_USER}/micronaut-app:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/${LOWERCASE_USER}/micronaut-app:latest" &
 wait
