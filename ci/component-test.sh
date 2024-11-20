f1119506fe9eff818dea9e5943a9a748ff047601
Kaloyan Ribarov <108590701+TheFisher1@users.noreply.github.com>
2024-11-20 17:41:07 +0200

    save file 3

--- ci/component-test.sh
+++ ci/component-test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 LOWERCASE_USER=$(echo "$GITHUB_USER" | tr '[:upper:]' '[:lower:]')
 
-[[ -z "${GITHUB_USER}" ]] && DockerRepo='' || DockerRepo="${LOWERCASE_USER}/"
+[[ -z "${LOWERCASE_USER}" ]] && DockerRepo='' || DockerRepo="${LOWERCASE_USER}/"
 docker compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
